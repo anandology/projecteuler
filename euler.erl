165501c7eb3b5884c3b07ab052232a019f3a81cb
Anand Chitipothu <anandology@gmail.com>
2009-04-04 13:48:52 +0530

    solutions to p8, p9, p10, p12, p14 in erlang.

--- euler.erl
+++ euler.erl
@@ -7,7 +7,14 @@
     p4/0, p4/1, reverse_number/1,
     p5/0, p5/1, sieve/1, max_power/2,
     p6/0, p6/1,
-    p7/0, p7/1
+    p7/0, p7/1,
+    p8/0,
+    p9/0, p9/1,
+    p10/0, p10/1,
+    p11/0, 
+    p12/0, p12/1, divisors/1,
+    p13/0,
+    p14/0, p14/1, maxValueIndex/1, collatzNumbers/1, collatzNumber/1
 ]).
 
 % 1. Find the sum of all the multiples of 3 or 5 below 1000.
@@ -91,10 +98,105 @@ p7(N, Current, Primes) ->
         true -> p7(N, 2*Current, Primes2)
     end.
 
+% 8. Find the greatest product of five consecutive digits in the 1000-digit number. (number stored in data/p008.txt)
+
+max(X, Y) when X > Y -> X;
+max(_, Y) -> Y.
+
+p8() -> p8("data/p008.txt").
+p8(Filename) -> 
+    {ok, Data} = file:read_file(Filename),
+    p8([C-$0 || C <- binary_to_list(Data), C /= $\n], 0).
+p8([C1, C2, C3, C4, C5|Rest], ProductSoFar) -> p8([C2, C3, C4, C5|Rest], max(ProductSoFar, C1*C2*C3*C4*C5));
+p8(_, ProductSoFar) -> ProductSoFar.
+
+% 9. There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc.
+
+head([H|_]) -> H.
+
+p9() -> p9(1000).
+p9(N) -> head([X*Y*Z || X <- lists:seq(1, N div 3), Y <- lists:seq(X, (1000-X) div 2), Z <- [1000-(X+Y)], X*X + Y*Y == Z*Z]).
+
+% 10. Find the sum of all the primes below two million.
+p10() -> p10(20000).
+p10(N) -> lists:sum(sieve(N)).
+
+% 11. What is the greatest product of four adjacent numbers in any direction 
+%     (up, down, left, right, or diagonally) in the 20x20 grid? (see data/p011.txt)
+
+p11() -> 0.
+
+% 12. What is the value of the first triangle number to have over five hundred divisors?
+
+divisors(N) -> divisors(N, 1, []).
+divisors(N, X, Result) when X*X > N -> Result;
+divisors(N, X, Result) when N rem X == 0 
+    -> divisors(N, X+1, [X|[N div X|Result]]);
+divisors(N, X, Result) -> divisors(N, X+1, Result).
+
+divisor_count(N) -> divisor_count(N, 1, 0).
+divisor_count(N, X, Count) when X*X > N -> Count;
+divisor_count(N, X, Count) when N rem X == 0
+    -> divisor_count(N, X+1, Count+2);
+divisor_count(N, X, Count) -> divisor_count(N, X+1, Count).
+
+p12() -> p12(500).
+p12(N) -> p12(1, 1, N).
+p12(Index, TriangularNumber, Count) -> 
+    DivisorCount = divisor_count(TriangularNumber),
+    if 
+        DivisorCount > Count -> TriangularNumber;
+        true -> p12(Index+1, TriangularNumber+Index+1, Count)
+    end.
+
+% 13. Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
+
+p13() -> 0.
+
+% 14. The following iterative sequence is defined for the set of positive integers:
+%       n = n/2 (n is even)
+%       n = 3n + 1 (n is odd)
+%     Which starting number, under one million, produces the longest chain?
+
+list_index(L, X) -> list_index(L, X, 0).
+list_index([], _, _) -> -1;
+list_index([Head|Rest], X, Index) -> 
+    case Head == X of
+        true -> Index;
+        false -> list_index(Rest, X, Index+1)
+    end.
+
+memoize_map(Fun, Values) -> lists:reverse(memoize_map(Fun, Values, [], array:new())).
+
+memoize_map(_, [], Results, _) -> Results;
+memoize_map(Fun, [Head|Rest], Results, Cache) ->
+    case array:get(Head, Cache) of
+        undefined -> 
+            Value = Fun(Head),
+            memoize_map(Fun, Rest, [Value|Results], array:set(Head, Value, Cache));
+        Value -> memoize_map(Fun, Rest, [Value|Results], Cache)
+    end. 
+    
+
+maxValueIndex(L) -> list_index(L, lists:max(L)).
+
+collatzNumber(1) -> 1;
+collatzNumber(N) -> 
+    case N rem 2 of
+        1 -> 1 + collatzNumber(3*N+1);
+        0 -> 1 + collatzNumber(N div 2)
+    end.
+
+collatzNumbers(N) -> memoize_map(fun collatzNumber/1, lists:seq(1, N)).
+
+p14() -> p14(1000000).
+p14(N) -> maxValueIndex(collatzNumbers(N)).
+
 run(Name) ->
     {Time, Answer} = timer:tc(euler, Name, []),
     io:format("~w\t~w\t~g~n", [Name, Answer, Time/1000000.0]).
 
 main(_) ->
-    lists:foreach(fun(Name)-> run(Name) end, [p1, p2, p3, p4, p5, p6, p7]).
+    %lists:foreach(fun(Name)-> run(Name) end, [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13]).
+    lists:foreach(fun(Name)-> run(Name) end, [p14]).
 
